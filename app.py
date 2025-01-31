32145c66e75f6e2acdc521ad96de96fb742d714d
pujianira <pujianira@gmail.com>
2025-01-31 11:41:37 +0700

    m

--- app.py
+++ app.py
@@ -4,8 +4,19 @@ import pandas as pd
 # 📂**1.Import & Load Dataset**🧩
 
 # **a. Import dataset**
-df = pd.read_csv('telkomsel.csv')
+# df = pd.read_csv('telkomsel.csv')
 
-# **b. Lima baris awal dari dataset**
-st.markdown("### Lima Baris Awal dari Dataset")
-st.write(df.head())
+# # **b. Lima baris awal dari dataset**
+# st.markdown("### Lima Baris Awal dari Dataset")
+# st.write(df.head())
+
+import os
+import streamlit as st
+
+file_path = 'telkomsel.csv'
+if os.path.exists(file_path):
+    st.write("File ditemukan!")
+    df = pd.read_csv(file_path)
+    st.write(df.head())
+else:
+    st.error(f"File {file_path} tidak ditemukan.")
