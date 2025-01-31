e19c4722e43cdfde6a20416d8f2ef2c353907d1e
pujianira <pujianira@gmail.com>
2025-01-31 11:39:44 +0700

    p

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-"""#  📂**1.Import & Load Dataset**🧩
+# 📂**1.Import & Load Dataset**🧩
 
-**a. Import dataset**
-"""
+# **a. Import dataset**
 df = pd.read_csv('telkomsel.csv')
 
-"""**b. Lima baris awal dari dataset**"""
+# **b. Lima baris awal dari dataset**
+st.markdown("### Lima Baris Awal dari Dataset")
 st.write(df.head())
